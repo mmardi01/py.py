7880936dbaf44b1d1a543d8c1e6c018898792ef7
mmardi01 <mmardi@student.1337.ma>
2024-12-15 00:30:17 +0100

    done with ex02

--- day00/ex02/find_ft_type.py
+++ day00/ex02/find_ft_type.py
@@ -1,15 +1,14 @@
 def all_thing_is_obj(object: any) -> int:
-    print(object,type(object))
-    return 42
 
-# ft_list = ["Hello", "tata!"]
-# ft_tuple = ("Hello", "toto!")
-# ft_set = {"Hello", "tutu!"}
-# ft_dict = {"Hello" : "titi!"}
-# all_thing_is_obj(ft_list)
-# all_thing_is_obj(ft_tuple)
-# all_thing_is_obj(ft_set)
-# all_thing_is_obj(ft_dict)
-# all_thing_is_obj("Brian")
-# all_thing_is_obj("Toto")
-# print(all_thing_is_obj(10))
+    list = ["list", "tuple", "set", "dict"]
+
+    if (type(object).__qualname__ in list):
+        print(type(object).__qualname__.capitalize(), ":", type(object))
+
+    elif (type(object).__qualname__ == "str"):
+        print(object, "is in the kitchen :", type(object))
+
+    else:
+            print("Type not found")
+            
+    return 42
