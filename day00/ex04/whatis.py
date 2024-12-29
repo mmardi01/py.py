8a611c140a6f6667692bf5efd48562506b6219b0
mmardi01 <mmardi@student.1337.ma>
2024-12-29 16:18:04 +0100

    fix ex04

--- day00/ex04/whatis.py
+++ day00/ex04/whatis.py
@@ -1,18 +1,24 @@
 import sys
 
+
 def is_odd_or_even(number):
-    try:
+    try:        
         number = int(sys.argv[1])
         if number % 2 == 0:
             print("I'm Even.")
         else:
             print("I'm Odd.")
-    except ValueError:
-         raise AssertionError("argument is not an integer")
+    except:
+        raise AssertionError('argument is not an integer')
 
 
+def main():
+    if len(sys.argv) > 2:
+        raise AssertionError("more than one argument is provided")
+    elif len(sys.argv) == 2:
+        is_odd_or_even(sys.argv[1])
 
-if len(sys.argv) > 2:
-      raise AssertionError("more than one argument is provided")
-elif len(sys.argv) == 2:
-    is_odd_or_even(sys.argv[1])
+try:
+    main()
+except AssertionError as error:
+    print(AssertionError.__name__,':', error)
