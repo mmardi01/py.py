a79bb726f63ef87f9d1d1ec7f4314030ec37f09f
mmardi01 <mmardi@student.1337.ma>
2024-12-29 17:13:44 +0100

    done with ex05

---
+++ day00/ex05/building.py
@@ -0,0 +1,48 @@
+import sys
+
+
+def count(text: str):
+    
+    punctuation_marks = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    punctuation_number = 0
+    upper = 0
+    lower = 0
+    spaces = 0
+    digits = 0
+    for s in text:
+        if s.islower():
+            lower += 1
+        if s.isupper():
+            upper += 1
+        if s.isspace():
+            spaces += 1
+        if s.isdigit():
+            digits += 1
+        if s in punctuation_marks:
+            punctuation_number += 1
+
+    print(f"The text contains {text.__len__()} characters:")
+    print(f"{upper} upper letters")
+    print(f"{lower} upper lower")
+    print(f"{spaces} spaces")
+    print(f"{digits} digits")
+
+def main():
+    try:
+        if sys.argv.__len__() > 2:
+            raise AssertionError('too many args')
+        elif sys.argv.__len__() == 1:
+            x = input('What is the text to count?\n')
+        else:
+            x = sys.argv[1]
+        count(x)
+    except AssertionError as error:
+        print(error)
+    except EOFError as error:
+        return;
+    except KeyboardInterrupt as error:
+        return;
+
+if __name__ == "__main__":
+    main()
+    
