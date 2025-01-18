980703e40cbef6c4d0577d548d6b11307d917408
mmardi01 <mmardi@student.1337.ma>
2025-01-18 18:36:22 +0100

    add __doc__ and fix norm

--- day00/ex08/tester.py
+++ day00/ex08/tester.py
@@ -2,11 +2,9 @@ from time import sleep
 from Loading import ft_tqdm
 from tqdm import tqdm
 
-for elem in ft_tqdm(range(10)):
-    sleep(1)
-
-print()
-for elem in tqdm(range(333)):
+for elem in ft_tqdm(range(333)):
     sleep(0.005)
 
-print()
+print(tqdm.__doc__)
+for elem in tqdm(range(333)):
+    sleep(0.005)
