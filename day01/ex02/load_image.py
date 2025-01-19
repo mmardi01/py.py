f9aa6ac523091fe7fd04d89588ffd198205f5101
mmardi01 <mmardi@student.1337.ma>
2025-01-19 23:37:46 +0100

    done with ex02 day01

--- day01/ex02/load_image.py
+++ day01/ex02/load_image.py
@@ -1,6 +1,22 @@
 from numpy import array
 import numpy as np
+from PIL import Image
+import PIL as pil
+
 
 def ft_load(path: str) -> array:
-    #your code here
-    return
+    '''
+    loads an image, prints its format, and its pixels
+    content in RGB format.
+    '''
+    try:
+        image = Image.open(path)
+        x = np.array(image)
+        print(x.shape)
+        return x
+    except FileNotFoundError:
+        print("File not found")
+    except pil.UnidentifiedImageError:
+        print('File is not an image')
+    except Exception:
+        print("Failed to read file")
