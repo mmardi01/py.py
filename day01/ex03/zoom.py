d5a83a544558a0b02b309f8161684b9e43e2eaac
mmardi01 <mmardi@student.1337.ma>
2025-01-22 02:23:51 +0100

    fix ex03d01 norm

--- day01/ex03/zoom.py
+++ day01/ex03/zoom.py
@@ -2,16 +2,16 @@ from load_image import ft_load
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-from skimage import io
+
 
 def main():
-    imageArray : np.array = ft_load('animal.jpeg')
-    grayScale = Image.fromarray(imageArray[100:100+400, 450:450+400]).convert("L")
-    grayArray = np.array(grayScale)
-    x = grayArray.reshape((400,400,1))
-    print("New shape after slicing:",x.shape, 'or', grayArray.shape)
+    imageArray = ft_load('animal.jpeg')
+    zoomedImage = Image.fromarray(imageArray[100:100+400, 450:450+400])
+    grayArray = np.array(zoomedImage.convert("L"))
+    x = grayArray.reshape((400, 400, 1))
+    print("New shape after slicing:", x.shape, 'or', grayArray.shape)
     print(x)
-    img = plt.imshow(x, cmap='gray')
+    plt.imshow(x, cmap='gray')
     plt.show()
 
 
