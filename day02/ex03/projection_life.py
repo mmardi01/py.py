34046424a08cae60089a539743ca0ef3b6805e3a
Mustapha Mardi <mustapha.mardi01@gmail.com>
2026-08-28 23:58:29 +0100

    fix day02 norm

--- day02/ex03/projection_life.py
+++ day02/ex03/projection_life.py
@@ -1,19 +1,24 @@
 from load_csv import load
 import matplotlib.pyplot as plt
-import numpy as np
+
+
 def main():
-    income_data = load('income_per_person_gdppercapita_ppp_inflation_adjusted.csv')
+    file_path = 'income_per_person_gdppercapita_ppp_inflation_adjusted.csv'
+    income_data = load(file_path)
     life_expectancy_data = load('life_expectancy_years.csv')
-    print(income_data['1900'].values)
-    print(life_expectancy_data['1900'].values)
-    plt.plot(income_data['1900'].values,
-        life_expectancy_data['1900'].values, 'ro')
+    incomin_data = income_data['1900'].values
+    life_expectancy = life_expectancy_data['1900'].values
+    print(incomin_data)
+    print(life_expectancy)
+    plt.plot(incomin_data, life_expectancy, 'go')
     plt.title("Year 1900")
     plt.xlabel("Gross domestic product")
     plt.ylabel("Life expectancy")
     plt.xscale('log')
     plt.xticks([300, 1000, 10000,], ['300', '1k', '10k'])
-    
+
     plt.show()
+
+
 if __name__ == '__main__':
     main()
